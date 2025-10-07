966b74c555ef62d26413372a8354f7deac655b7c
unknown <sara.kluge@hotmail.com>
2025-10-07 11:21:43 +0200

    add basics to Teacher class

--- src/Teacher.java
+++ src/Teacher.java
@@ -1,8 +1,16 @@
 public class Teacher extends Person {
     private int experienceYear;
 
-    public Teacher(String name, int experienceYear) {
+    public Teacher(String name, String securityNumber, String email, int experienceYear) {
+        super(name, securityNumber, email);
+        this.experienceYear = experienceYear;
+    }
 
+    public int getExperienceYear() {
+        return experienceYear;
+    }
+
+    public void setExperienceYear(int experienceYear) {
         this.experienceYear = experienceYear;
     }
 }
