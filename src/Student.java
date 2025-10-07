2c40ca6317fa60ce135b5146f44cb79cc301bede
Hugo Leijonhufvud <thechosenwolf@outlook.com>
2025-10-07 12:49:26 +0200

    Here is the actually student Class

--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+public class Student extends Person{
+    private int classYear;
+    
+    public Student(String name, String securityNumber, String email, int classYear) {
+        super(name, securityNumber, email);
+        this.classYear = classYear;
+    }
 
-public class Student {
+    public int getClassYear() {
+        return classYear;
+    }
+
+    public void setClassYear(int classYear) {
+        this.classYear = classYear;
+    }
 }
