872e5b0348c58eb1fc9c4c16059f46fd8ca59ca8
unknown <sara.kluge@hotmail.com>
2025-10-07 11:16:47 +0200

    WIP

---
+++ src/Teacher.java
@@ -0,0 +1,8 @@
+public class Teacher extends Person {
+    private int experienceYear;
+
+    public Teacher(String name, int experienceYear) {
+
+        this.experienceYear = experienceYear;
+    }
+}
