d2f054fec09b21156fc90fde692d1647bd0d4115
Dimage Sapelkin <kerabromsmu@users.noreply.github.com>
2025-10-08 16:26:55 +0200

    * TextMenu cleanup

--- src/Helpers/TextMenu.java
+++ src/Helpers/TextMenu.java
@@ -62,19 +62,6 @@ public class TextMenu {
 
     public static <T extends Named> void listMenuLoop(String header, String exit, String emptyListMessage, List<T> list, Consumer<T> choiceCallback, boolean singleShot) {
         listMenuLoop(header,exit,emptyListMessage, () -> list, choiceCallback, singleShot);
-/*        if (list.isEmpty()) {
-            System.out.println(emptyListMessage);
-            return;
-        }
-        String[] options = new String[list.size() + 1];
-        options[0] = exit;
-        Runnable[] callbacks = new Runnable[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            T item = list.get(i);
-            options[i+1] = item.getName();
-            callbacks[i] = () -> choiceCallback.accept(item);
-        }
-        menuLoop(header, options, callbacks, singleShot);*/
     }
 
     public static boolean yesNoQuestion(String question) {
